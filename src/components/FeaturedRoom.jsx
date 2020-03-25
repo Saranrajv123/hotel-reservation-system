ec7a69d90e471982178327053e3df96ea177867b
Saranrajv123 <vsaranraj16@gmail.com>
2020-03-25 23:01:52 +0530

    added data and working on featured Component 2-06

--- src/components/FeaturedRoom.jsx
+++ src/components/FeaturedRoom.jsx
@@ -2,19 +2,22 @@ import React, { Fragment, useContext, useEffect } from 'react'
 import RoomContext  from '../Context';
 
 
-export const FeaturedRoom = () => {
-    const context = useContext(RoomContext)
+export const FeaturedRoom = (props) => {
+    // const {featuredRoom} = useContext(RoomContext)
+    const value = useContext(RoomContext)
     // const value = this.context;
     // console.log('contextType',contextType)
 
+    console.log('props.featuredRoom', props.featuredRooms)
+
     useEffect(() => {
-       console.log('conte', context)
+       console.log('conte', value)
     }, [])
 
     return (
        <Fragment>
            <div>
-               Hello from featured Component {context}
+               Hello from featured Component
            </div>
        </Fragment>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,58 @@
-import React, { Fragment, Consumer, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Hero } from '../components/Hero';
 import { Banner } from '../components/Banner';
 import { Link } from 'react-router-dom';
 import { Services } from '../components/Services';
 import { FeaturedRoom } from '../components/FeaturedRoom';
 import { RoomProvider } from '../Context';
+import RoomsData from "../RoomsData";
 
-const RoomContext = React.createContext();
+// const RoomContext = React.createContext();
 
 const Home = () => {
 
-    const [state, setstate] = useState("Hello");
-    const [greeting, setGreeting] = useState("greeting");
+    const [rooms, setRooms] = useState([]);
+    const [sortedRooms, setSortedRooms] = useState([]);
+    const [featuredRoom, setFeaturedRoom] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        let room = formatRoomsData(RoomsData);
+        let featuredRoom = room.filter(item => item.featured === true);
+        setFeaturedRoom(featuredRoom);
+        setRooms(room);
+        setSortedRooms(room);
+        setLoading(false);
+    },[])
+
+    // console.log('rooms', rooms)
+
+    const formatRoomsData = (roomsDataCollection) => {
+        console.log('object', roomsDataCollection);
+        let roomData = roomsDataCollection.map(item => {
+            let id = item.sys.id;
+            let getImages = getRoomsImages(item);
+            const rooms = {...item.fields, images:getImages, id}
+            return rooms;
+        })
+        return roomData;
+    }
+
+    const getRoomsImages = (item) => {
+        return item.fields.images.map(data => data.fields.file.url);
+    }
 
     return (
         <Fragment>
             <div>
-
                 <Hero>
                     <Banner title="luxurious rooms" subTitle="Deluxe rooms staring at $299">
                         <Link to="/rooms" className="btn-primary">our rooms</Link>
                     </Banner>
                 </Hero>
                 <Services />
-                <RoomProvider value={state, greeting}>
-                    <FeaturedRoom />
+                <RoomProvider value={rooms}>
+                    <FeaturedRoom featuredRooms={featuredRoom}/>
                 </RoomProvider>
             </div>
         </Fragment>
@@ -32,4 +60,4 @@ const Home = () => {
     )
 }
 
-export { Home, RoomContext }
+export { Home }
